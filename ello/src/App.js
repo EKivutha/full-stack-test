ad6c702d967ec2693254b1dd384860a341d30064
Ezra <gerkibz@outlook.com>
2022-04-09 08:36:34 +0300

    app styles, main file and test file add

---
+++ ello/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+import Books from './books/reading';
+
+
+const App = () => (
+    <div>
+      <Books/>
+    </div>
+);
+
+export default App;
