08840d7d89bedf669e1d4405b510c4a61edafa37
Ezra <gerkibz@outlook.com>
2022-04-09 16:24:11 +0300

    add update page navigation page

--- ello/src/books/reading.js
+++ ello/src/books/reading.js
@@ -4,32 +4,21 @@ import GET_BOOKS from '../Queries/bookQueris'
 import { useQuery } from '@apollo/client';
 
 
+//display indivudual pages as per id
+//each word match to token
+
 function Books() {
   const { loading, error, data } = useQuery(GET_BOOKS);
-  console.log(data)
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :( ${error.message}`</p>;
 
-  if (data) return (
-    <div>
+  if (loading) return 'Loading...';
+  else if (error) return `Error! ${error.message}`;
 
-      <h1>{data.book.title}</h1>
-      <h2>{data.book.author}</h2>
-      <div> {data.book.pages.map(page => (
-        <option key={page.id} value={page.id}>
-          {page.content}
-          <div> {data.book.pages.tokens.map(token => (
-            <option key={token.id} value={token.id}>
-              {token.position}
-              {token.value}
-
-            </option>
-          ))}</div>
-        </option>
-      ))}</div>
-      {/* <p>{data.book.pages}</p> */}
-    </div>
-  );
+  else if (data) {
+    console.log(data)
+    return (
+      console.log(data.book.pages)
+    )
+  }
 }
 
-export default Books;
+export default Books
